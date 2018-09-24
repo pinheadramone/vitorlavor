cb473714c41b627d43a3e1f4bd033d244bb1c76c
root <vitor.lavor@hotmail.com>
2018-09-24 16:03:18 -0300

    Adicionando

--- app/index.py
+++ app/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Testando Git!!!!!'
+    return 'Aplicação em Flask!!!!!'
 if __name__ == '__main__':
      app.debug = True
      app.run(host='0.0.0.0')
